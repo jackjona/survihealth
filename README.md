e86759eb1a376b089845f364f5cf4b1b2a489de9
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-08-10 10:38:59 -0400

    production

---
+++ README.md
@@ -0,0 +1,39 @@
+# Health
+
+A Health Care Website Built With Gatsby.
+
+
+## View The Live Site [Here](https://health.jackjona.live)
+
+## Software
+
+- React
+- Gatsby
+- Gatsby Plugins
+- material-ui
+
+### To Locally Develop
+
+1. Install the Gatsby CLI.
+
+   ```bash
+   npm install -g gatsby-cli
+   ```
+2. 
+   ```bash
+   git clone https://github.com/jackjona123/health.git
+   cd SurviHealth-gatsby
+   npm install
+   ```
+
+3.
+   ```bash
+   gatsby develop
+   ```
+
+4. To Deploy:
+
+   ```bash
+   gatsby build
+   ```
+
---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `SurviHealth`,
+    description: `We provide progressive, and affordable healthcare, accessible on mobile and online for everyone`,
+    author: `Jack Jona`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/survihealth-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-material-ui`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
